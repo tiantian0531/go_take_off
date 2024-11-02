e8aa5cab6225c405e3faeb4c1818258bd89ac595
yu_xianglong <87717663@qq.com>
2024-11-02 22:50:02 +0800

    array transfer test

--- go_take_off/03.Array_Import/main.go
+++ go_take_off/03.Array_Import/main.go
@@ -16,18 +16,35 @@ func main() {
 	// avg := sum / 5
 	// fmt.Printf("成绩的总和是：%v,成绩的平均数是：%v", sum, avg)
 
-	var scores [5]int
-	scores[0] = 95
-	scores[1] = 91
-	scores[2] = 39
-	scores[3] = 60
-	scores[4] = 21
+	// var scores [5]int
+	// scores[0] = 95
+	// scores[1] = 91
+	// scores[2] = 39
+	// scores[3] = 60
+	// scores[4] = 21
 
-	var sum = 0
-	for i := 0; i < len(scores); i++ {
-		sum += scores[i]
-	}
-	avg := sum / len(scores)
-	fmt.Printf("容量是：%v,长度是%v", cap(scores), len(scores))
-	fmt.Printf("成绩的总和是：%v,成绩的平均数是：%v", sum, avg)
+	// var sum = 0
+	// for i := 0; i < len(scores); i++ {
+	// 	sum += scores[i]
+	// }
+	// avg := sum / len(scores)
+	// fmt.Printf("容量是：%v,长度是%v", cap(scores), len(scores))
+	// fmt.Printf("成绩的总和是：%v,成绩的平均数是：%v", sum, avg)
+
+	//引用指针传递
+	var arr_ref = [3]int{1, 2, 3}
+	assignment_ref(&arr_ref)
+	fmt.Println(arr_ref)
+
+	//值传递
+	var arr_val = [3]int{7, 8, 9}
+	assignment_val(arr_val)
+	fmt.Println(arr_val)
+}
+
+func assignment_ref(arr *[3]int) {
+	arr[0] = 999
+}
+func assignment_val(arr [3]int) {
+	arr[0] = 888
 }
--- go_take_off/04.Array_Memory_Alyanis/main.go
+++ go_take_off/04.Array_Memory_Alyanis/main.go
@@ -4,6 +4,11 @@ import "fmt"
 
 func main() {
 	var scores [5]int
+	scores[0] = 95
+	scores[1] = 91
+	scores[2] = 39
+	scores[3] = 60
+	scores[4] = 21
 	//声明未赋值，所有值为0
 	for i := 0; i < len(scores); i++ {
 		fmt.Printf("%v", scores[i])
@@ -20,6 +25,7 @@ func main() {
 	fmt.Println()
 	fmt.Printf("数组第三个空间的地址是：%p", &scores[2])
 
+	fmt.Printf("数组第四个空间的地址是：%p", &scores[3])
 	// 	00000
 	// 数组的地址是：0xc000010390
 	// 数组第一个空间的地址是：0xc000010390
