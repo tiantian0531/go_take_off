09e03bb6289ec19172d247ad7f0423338e14d036
yu_xianglong <87717663@qq.com>
2024-12-05 23:22:26 +0800

    add oop demo

--- go_take_off/11.Oop_First/main.go
+++ go_take_off/11.Oop_First/main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
+type User struct {
+	Name   string
+	Age    int
+	School string
+}
+
 func main() {
 
+	// var t1 User
+	t1 := &User{Name: "sa"}
+	fmt.Println(t1)
+
+	t1.Name = "张三"
+	fmt.Println(t1.Name)
 }
