1d20221bccfc9367af166b6a1f96b21dcdde51b7
yu_xianglong <87717663@qq.com>
2024-12-26 22:18:43 +0800

    add reflect demo

---
+++ go_take_off/16.Reflect/OInstance/OInstance.go
@@ -0,0 +1,88 @@
+package oinstance_test
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Student struct {
+	Name string
+	Age  int
+}
+
+func (s Student) Print() {
+	fmt.Println("调用了Print方法")
+	fmt.Println("学生的名字是", s.Name)
+}
+
+func (s Student) GetSumn(n1, n2 int) int {
+	return n1 + n2
+}
+
+func (s *Student) Set(name string, age int) {
+	s.Name = name
+	s.Age = age
+}
+
+func OperStundentStruct(a interface{}) {
+	val := reflect.ValueOf(a)
+	fmt.Println("val的值的", val)
+
+	//通过val.Value类型操作结构体内部字段
+	typ := reflect.TypeOf(a)
+	fields := val.Elem().NumField()
+
+	for i := 0; i < fields; i++ {
+		fmt.Printf("第%d个字段名为%v,值为 %v;\n", i, typ.Elem().Field(i).Name, val.Elem().Field(i))
+	}
+
+	methods := val.Elem().NumMethod()
+
+	for i := 0; i <= methods; i++ {
+		fmt.Printf("第%d个方法是 %v;\n", i, typ.Method(i).Name)
+		fmt.Printf("第%d个方法是 %v,参数有：%v \n", i, typ.Method(i).Name, typ.Method(i).Type.NumIn())
+
+	}
+
+	var params []reflect.Value
+
+	params = append(params, reflect.ValueOf("Q"))
+	params = append(params, reflect.ValueOf(20))
+	val.Method(2).Call(params)
+
+	fmt.Println("", val.Interface().(*Student).Name)
+	//通过val.Value类型操作结构体内部方法
+
+}
+
+func OperStundentSetStruct(a interface{}) {
+	val := reflect.ValueOf(a)
+	fmt.Println("val的值的", val)
+
+	//通过val.Value类型操作结构体内部字段
+	typ := reflect.TypeOf(a)
+	fields := val.Elem().NumField()
+
+	for i := 0; i < fields; i++ {
+		fmt.Printf("第%d个字段名为%v,值为 %v;\n", i, typ.Elem().Field(i).Name, val.Elem().Field(i))
+
+	}
+	val.Elem().Field(0).SetString()
+	methods := val.Elem().NumMethod()
+
+	for i := 0; i <= methods; i++ {
+		fmt.Printf("第%d个方法是 %v;\n", i, typ.Method(i).Name)
+		fmt.Printf("第%d个方法是 %v,参数有：%v \n", i, typ.Method(i).Name, typ.Method(i).Type.NumIn())
+
+	}
+
+	var params []reflect.Value
+
+	params = append(params, reflect.ValueOf("Q"))
+	params = append(params, reflect.ValueOf(20))
+	val.Method(2).Call(params)
+
+	fmt.Println("", val.Interface().(*Student).Name)
+	//通过val.Value类型操作结构体内部方法
+
+}
---
+++ go_take_off/16.Reflect/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	oinstance_test "first/16.Reflect/OInstance"
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	reflect03()
+}
+
+func Reflect01() {
+	var i int = 2
+
+	//获取类型
+	var t = reflect.TypeOf(i)
+	fmt.Printf("%T", t)
+
+	//过去值类型
+	v := reflect.ValueOf(i)
+	fmt.Printf("%T \n", v)
+
+	p := v.Interface()
+	n := p.(int)
+
+	fmt.Println("最终值为", n+5)
+
+	newV := 3
+	v1 := reflect.ValueOf(&i)
+
+	v1.Elem().SetInt(int64(newV))
+
+	fmt.Println("...", i)
+}
+
+func reflect02() {
+	stu := oinstance_test.Student{
+		Name: "小米",
+		Age:  18,
+	}
+
+	fmt.Println("", reflect.TypeOf(stu))
+	fmt.Println("", reflect.ValueOf(stu))
+	fmt.Println("", reflect.TypeOf(stu).Kind())
+	fmt.Println("", reflect.ValueOf(stu).Kind())
+
+	v := reflect.ValueOf(stu).Interface()
+	a := v.(oinstance_test.Student)
+	fmt.Println("", a)
+}
+
+func reflect03() {
+	stu := &oinstance_test.Student{
+		Name: "小米1",
+		Age:  20,
+	}
+
+	oinstance_test.OperStundentStruct(stu)
+}
